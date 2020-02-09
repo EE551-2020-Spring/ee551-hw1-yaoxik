a4b226cf3d9f286e82b379b2c6236c9f5167a83b
yaoxikang <56726542+yaoxikang@users.noreply.github.com>
2020-02-09 13:22:12 -0500

    Add files via upload

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -14,15 +14,15 @@ def numbers_and_strings():
     x = "EE551"
 
     # Assign a string "Stevens" to a variable y
-
+    y = "Stevens"
     # Repeat variable y 5 times
-
+    z = y*5
     # What is the length of z?
-
+    length = len(z)
     # Concatenate variable y with string " is good"
-
+    m = y + "is good"
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
-
+    n=y.replace("good","awesome")
     return x, y, z, length, m, n
 
 
@@ -33,14 +33,16 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
-
+    p = n.string()
     # Get all the items past the first of the third substring
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-
+    c = [[1,4,5],
+        [6,10,11],
+        [12,17,38]]
     # Collect the items in the last column of matrix A using list comprehension
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
